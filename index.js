60a4f96959a6e8309a04a6e16c12df0aeef5a4a5
MASUD-01 <masudrana96141@gmail.com>
2022-05-10 14:40:20 +0600

    update api create

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 require("dotenv").config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jh56u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -28,6 +28,38 @@ async function run() {
             const cursor = carsCollection.find(query);
             const products = await cursor.limit(6).toArray();
             res.send(products)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await carsCollection.findOne(query);
+            res.send(service);
+        });
+
+        // update product
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            console.log(updatedProduct)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+
+                    quantity: updatedProduct.quantity
+
+                }
+            };
+            const result = await carsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        })
+        // delete a user
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carsCollection.deleteOne(query);
+            res.send(result);
         })
     } finally {
 
